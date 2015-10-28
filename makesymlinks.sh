1c8892ae8d78382aab4ad9b6b158dc38da354335
James Best <jim-at-jibba@Jamess-Mac-mini.default>
2015-10-28 21:08:08 +0000

    Added vimrc and tmux theme

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="oh-my-zsh zshrc tmux.conf"    # list of files/folders to symlink in homedir
+files="vimrc oh-my-zsh zshrc tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
