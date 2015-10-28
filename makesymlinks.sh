015e10783c7781f4d5eb97c37db85a209ff8ffb8
James Best <jim-at-jibba@Jamess-Mac-mini.default>
2015-10-28 09:47:49 +0000

    Added zshrc and oh-my-zsh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc tmux.conf"    # list of files/folders to symlink in homedir
+files="oh-my-zsh zshrc tmux.conf"    # list of files/folders to symlink in homedir
 
 ##########
 
