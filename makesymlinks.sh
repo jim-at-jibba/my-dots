46268366b61529481d5c311f61c0c1e75268b6f7
James Best <james.best@gravitywell.co.uk>
2016-08-26 08:34:00 +0100

    Added more dots and utility scripts

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc zsh zshrc tmux.conf scss-lint.yml vim eslintrc.js"    # list of files/folders to symlink in homedir
+files="vimrc zsh zshrc tmux.conf scss-lint.yml vim eslintrc.js csscomb.json"    # list of files/folders to symlink in homedir
 
 ##########
 
