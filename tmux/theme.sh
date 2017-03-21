060c56f9383988e40a79e0b408b63291a247f0c4
James Best <jimgbest@jibbadesign.co.uk>
2017-03-21 14:14:11 +0000

    Before the next crash

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -30,20 +30,20 @@ set-window-option -g window-status-bg default
 set -g window-status-format "#I #W"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
+set-window-option -g window-status-current-fg $tm_color_feature
 set-window-option -g window-status-current-bg default
 set-window-option -g  window-status-current-format "#[bold]#I #W"
 
 # pane border
 set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_color_active
+set-option -g pane-active-border-fg $tm_color_session
 
 # message text
 set-option -g message-bg default
 set-option -g message-fg $tm_color_active
 
 # pane number display
-set-option -g display-panes-active-colour $tm_color_active
+set-option -g display-panes-active-colour $tm_color_session
 set-option -g display-panes-colour $tm_color_inactive
 
 # clock
--- zshrc
+++ zshrc
@@ -4,7 +4,9 @@ export ZSH=$HOME/.oh-my-zsh
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded. pygmalion
-ZSH_THEME="sorin"
+ZSH_THEME="spaceship"
+
+SPACESHIP_TIME_SHOW=true
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
 
