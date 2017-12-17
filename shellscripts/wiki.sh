49a444c096d74422e8ba6bb86994bac7f9fdecff
jim-at-jibba <jim@justjibba.net>
2017-12-17 21:10:00 +0000

    Added vim wiki cheatsheet

---
+++ shellscripts/wiki.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+printf "[number] <leader> ww |	open wiki index file"
+printf "<leader> ws	| list and select available wiki"
+printf "[number] <leader> wi	| open diary index file for wiki"
+printf "<leader> w <leader> i	| update current diary index"
+printf "[number] <leader> w <leader> w	open | today’s diary file for wik"
