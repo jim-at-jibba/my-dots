1c47aad09542592e3e8329e222d9aeaaf6dcf047
James Best <jim@justjibba.net>
2020-04-14 17:37:09 +0100

    Update parrot/kali setup script

--- parrot/parrot_terminal_setup.sh
+++ parrot/parrot_terminal_setup.sh
@@ -21,7 +21,19 @@ echo "Install neovim........"
 sudo apt-get install neovim python3-neovim
 
 echo "Install others fzf etc......."
-sudo apt-get install fzf ripgrep fonts-powerline exa bat tmux
+sudo apt-get install fzf fonts-powerline tmux terminator
+
+echo "Installing ripgrep"
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+
+echo "Installing bat"
+curl -LO https://github.com/sharkdp/bat/releases/download/v0.13.0/bat_0.13.0_amd64.deb
+sudo dpkg -i bat_0.13.0_amd64.deb
+
+echo "Installing vim plug......"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Setting up sane defaults for tmux......"
 git clone https://github.com/gpakosz/.tmux.git
@@ -34,7 +46,7 @@ curl  https://raw.githubusercontent.com/jim-at-jibba/my-dots/master/nvim/init.vi
 curl  https://raw.githubusercontent.com/jim-at-jibba/my-dots/master/tmux.conf --output tmux.conf
 
 echo "Symlinking config"
-ln -s ~/$(USER)/dotfiles/init.vim ~/.config/nvim
+ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf.local
 
 echo "Installing Node"
