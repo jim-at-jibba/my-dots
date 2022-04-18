51f34bdb03ae24bc5f9d416b07ce087090967163
James Best <jim@justjibba.net>
2022-04-18 19:18:47 +0100

    Starting new neovim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,50 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+-- returns the require for use in `config` parameter of packer's use
+-- expects the name of the config file
+function get_config(name)
+	return string.format('require("config/%s")', name)
+end
+
+-- bootstrap packer if not installed
+if fn.empty(fn.glob(install_path)) > 0 then
+	fn.system({
+		"git",
+		"clone",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	execute("packadd packer.nvim")
+end
+
+-- initialize and configure packer
+local status_ok, packer = pcall(require, "packer")
+
+if not status_ok then
+    return
+end
+
+packer.init({
+	enable = true, -- enable profiling via :PackerCompile profile=true
+	threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+    display = {
+        open_fn = function()
+            return require("packer.util").float({ border = "single" })
+        end,
+    },
+})
+
+packer.startup(function(use)
+	-- actual plugins list
+	use("wbthomason/packer.nvim")
+
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+		config = get_config("telescope"),
+	})
+
+end)
