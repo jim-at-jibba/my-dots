234200f7614566a8e76d7c0f6d4ec18bcd3212bf
James Best <jim@justjibba.net>
2022-04-28 14:57:47 +0100

    Add new treesitter incremental select keymap

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,35 +1,44 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 
 if not status_ok then
-    return
+	return
 end
 
-configs.setup {
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    indent = {
-      enable = true
-    },
-    highlight = {
-      enable = true,              -- false will disable the whole extension
-      disable = { "c", "rust" },  -- list of language that will be disabled
-    },
-   -- refactor = {
-   --   highlight_definitions = {
-   --       enable = true
-   --   }
-   -- },
-     rainbow = {
-      enable = true,
-      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-      max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      -- colors = {}, -- table of hex strings
-      -- termcolors = {} -- table of colour name strings
-      },
-      autotag = {
-        enable = true
-      },
-      matchup = {
-        enable = true,              -- mandatory, false will disable the whole extension
-      },
-  }
+configs.setup({
+	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "gnn",
+			node_incremental = "grn",
+			scope_incremental = "grc",
+			node_decremental = "grm",
+		},
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "c", "rust" }, -- list of language that will be disabled
+	},
+	-- refactor = {
+	--   highlight_definitions = {
+	--       enable = true
+	--   }
+	-- },
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+	autotag = {
+		enable = true,
+	},
+	matchup = {
+		enable = true, -- mandatory, false will disable the whole extension
+	},
+})
