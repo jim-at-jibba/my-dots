3bb9aac852bd64e4fe455870d54e484a5841cf3f
James Best <jim@justjibba.net>
2022-07-14 21:37:02 +0100

    Adjust barbar

--- nvim/lua/config/barbar.lua
+++ nvim/lua/config/barbar.lua
@@ -3,13 +3,13 @@ vim.g.bufferline = {
 	animation = true,
 
 	-- Enable/disable auto-hiding the tab bar when there is a single buffer
-	auto_hide = false,
+	auto_hide = true,
 
 	-- Enable/disable current/total tabpages indicator (top right corner)
 	tabpages = true,
 
 	-- Enable/disable close button
-	closable = false,
+	closable = true,
 
 	-- Enables/disable clickable tabs
 	--  - left-click: go to buffer
