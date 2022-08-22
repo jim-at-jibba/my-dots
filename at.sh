754f50ede6ab948da9d6e27f6c9571bd08236759
James Best <jim@justjibba.net>
2022-08-22 21:29:43 +0100

    Add dark mode changing on theme change

--- at.sh
+++ at.sh
@@ -28,48 +28,58 @@ case $color in
   rosepinemoon)
     configure_kitty
     configure_tmux
-    # configure_vim 'vim.cmd("set background=dark")\nvim.cmd("colorscheme rose-pine")'
     configure_vim 'vim.cmd("set background=dark")'
+    dark-mode on
+    # configure_vim 'vim.cmd("set background=dark")\nvim.cmd("colorscheme rose-pine")'
     ;;
   rosepinedawn)
     configure_kitty
     configure_tmux
-    # configure_vim 'vim.cmd("set background=light")\nvim.cmd("colorscheme rose-pine")'
     configure_vim 'vim.cmd("set background=light")'
+    dark-mode off
+    # configure_vim 'vim.cmd("set background=light")\nvim.cmd("colorscheme rose-pine")'
     ;;
   catppuccinfrappe)
     configure_kitty
-    # configure_tmux
+    configure_tmux
     configure_vim 'vim.cmd("set background=dark")\nvim.g.catppuccin_flavour = "frappe"\nvim.cmd("colorscheme catppuccin")'
+    dark-mode on
     ;;
   catppuccinlatte)
     configure_kitty
-    # configure_tmux
+    configure_tmux
     configure_vim 'vim.cmd("set background=light")\nvim.g.catppuccin_flavour = "latte"\nvim.cmd("colorscheme catppuccin")'
+    dark-mode off
     ;;
   tokyolight)
     configure_alacritty
     configure_kitty
     configure_tmux
     configure_vim 'vim.cmd("set background=light")\nvim.cmd("colorscheme tokyonight")'
+    dark-mode off
     ;;
   tokyonight)
     configure_alacritty
     configure_kitty
     configure_tmux
     configure_vim 'vim.cmd("set background=dark")\nvim.cmd("colorscheme tokyonight")'
+    dark-mode on
     ;;
   nightfoxnord)
     configure_kitty
+    configure_tmux
     configure_vim 'vim.cmd("set background=dark")\nvim.cmd("colorscheme nordfox")'
+    dark-mode on
     ;;
   nightfoxday)
     configure_kitty
     configure_vim 'vim.cmd("set background=light")\nvim.cmd("colorscheme dayfox")'
+    dark-mode off
     ;;
   nightfoxdawn)
     configure_kitty
     configure_vim 'vim.cmd("set background=light")\nvim.cmd("colorscheme dawnfox")'
+    dark-mode on
     ;;
   *)
     echo "Supported colorschemes: tokyolight, tokyonight, rose pine moon, rose pine dawn, nightfox nord, nightfox day"
