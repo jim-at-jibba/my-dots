f4a6f8658896cbc2bfd50ca24c746af62c62fedb
James Best <jamesbest@Jamess-MacBook-Pro.local>
2022-08-28 14:06:58 +0100

    First pass at office tmux script

---
+++ shellscripts/tmux/office-stats/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  extends: ["plugin:prettier/recommended"],
+  env: {
+    node: true,
+  },
+};
