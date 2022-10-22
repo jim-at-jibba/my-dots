22c81a1f75f9ca5c924e45119106879ef1bc4188
James Best <jim@justjibba.net>
2022-10-22 06:36:30 +0100

    Fix lsp api changes

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -24,7 +24,7 @@ end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local servers = {
 	"gopls",
