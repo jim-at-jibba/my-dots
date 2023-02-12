c766bc743062e1a8bd4379b65daee7b5628c2c77
James Best <jamesbest@Jamess-MacBook-Pro.local>
2023-02-12 21:55:17 +0000

    Move to mason

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -68,204 +68,169 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- })
 
 local servers = {
-	"gopls",
-	"pyright",
-	"prismals",
-	"html",
-	"cssls",
-	"sumneko_lua",
-	"tsserver",
-	"yamlls",
-	"jsonls",
-	"marksman",
-	"sqlls",
-	"emmet_ls",
-	"astro",
-	"tailwindcss",
-	"grammarly",
-	-- "pylsp",
-}
-
--- Use a loop to conveniently call 'setup' on multiple servers
-for _, lsp in ipairs(servers) do
-	nvim_lsp[lsp].setup({
-		on_attach = function(client)
-			-- disable formatting for LSP clients as this is handled by null-ls
-			client.server_capabilities.documentFormattingProvider = false
-			client.server_capabilities.documentRangeFormattingProvider = false
-			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-			end
-			vim.diagnostic.config({ virtual_text = false })
-
-			if client.server_capabilities.documentFormattingProvider then
-				vim.cmd([[augroup Format]])
-				vim.cmd([[autocmd! * <buffer>]])
-				vim.cmd([[autocmd BufWritePost <buffer> lua require'lsp.formatting'.format()]])
-				vim.cmd([[augroup END]])
-			end
-
-			if lsp == "tsserver" then
-				require("nvim-lsp-ts-utils").setup({})
-			end
-
-			map("n", "<leader>dd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-			map("n", "<leader>d", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-
-			vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
-			-- diagnotics on hover - disabled in favor of lspsaga
-			-- vim.api.nvim_create_autocmd("CursorHold", {
-			-- 	buffer = bufnr,
-			-- 	callback = function()
-			-- 		local opts = {
-			-- 			focusable = false,
-			-- 			close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-			-- 			border = "rounded",
-			-- 			source = "always",
-			-- 			prefix = " ",
-			-- 			scope = "cursor",
-			-- 		}
-			-- 		vim.diagnostic.open_float(nil, opts)
-			-- 	end,
-			-- })
-		end,
-		before_init = function(_, config)
-			if lsp == "pyright" then
-				config.settings.python.pythonPath = get_python_path(config.root_dir)
-			end
-		end,
-		capabilities = capabilities,
-		settings = {
-			html = {
-				filetypes = {
-					"html",
-					"tmpl",
-				},
+	gopls = {
+		analyses = { unusedparams = true },
+		staticcheck = true,
+	},
+	pyright = {},
+	prismals = {},
+	html = {
+		filetypes = {
+			"html",
+			"tmpl",
+		},
+	},
+	cssls = {},
+	tsserver = {},
+	yamlls = {
+		schemaStore = {
+			enable = true,
+			url = "https://www.schemastore.org/api/json/catalog.json",
+		},
+		schemas = {
+			kubernetes = "*.yaml",
+			["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+			["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+			["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+			["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+			["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+			["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+			["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+			["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+			["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+			["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+			["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+			["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+		},
+		format = { enabled = false },
+		validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
+		completion = true,
+		hover = false,
+	},
+	jsonls = {
+		format = { enabled = false },
+		schemas = {
+			{
+				description = "ESLint config",
+				fileMatch = { ".eslintrc.json", ".eslintrc" },
+				url = "http://json.schemastore.org/eslintrc",
 			},
-			grammarly = {
-				filetypes = { "markdown", "text" },
-				init_options = {
-					clientId = "clientId=client_2zkt1YpZ613xS1H6m68nsE",
-				},
+			{
+				description = "Package config",
+				fileMatch = { "package.json" },
+				url = "https://json.schemastore.org/package",
 			},
-			emmet_ls = {
-				filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
-				init_options = {
-					html = {
-						options = {
-							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-							["bem.enabled"] = true,
-						},
-					},
-				},
+			{
+				description = "Packer config",
+				fileMatch = { "packer.json" },
+				url = "https://json.schemastore.org/packer",
 			},
-			gopls = { analyses = { unusedparams = true }, staticcheck = true },
-			json = {
-				format = { enabled = false },
-				schemas = {
-					{
-						description = "ESLint config",
-						fileMatch = { ".eslintrc.json", ".eslintrc" },
-						url = "http://json.schemastore.org/eslintrc",
-					},
-					{
-						description = "Package config",
-						fileMatch = { "package.json" },
-						url = "https://json.schemastore.org/package",
-					},
-					{
-						description = "Packer config",
-						fileMatch = { "packer.json" },
-						url = "https://json.schemastore.org/packer",
-					},
-					{
-						description = "Renovate config",
-						fileMatch = {
-							"renovate.json",
-							"renovate.json5",
-							".github/renovate.json",
-							".github/renovate.json5",
-							".renovaterc",
-							".renovaterc.json",
-						},
-						url = "https://docs.renovatebot.com/renovate-schema",
-					},
-					{
-						fileMatch = { "tsconfig*.json" },
-						url = "https://json.schemastore.org/tsconfig.json",
-					},
-					{
-						fileMatch = {
-							".prettierrc",
-							".prettierrc.json",
-							"prettier.config.json",
-						},
-						url = "https://json.schemastore.org/prettierrc.json",
-					},
-					{
-						description = "OpenApi config",
-						fileMatch = { "*api*.json" },
-						url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
-					},
+			{
+				description = "Renovate config",
+				fileMatch = {
+					"renovate.json",
+					"renovate.json5",
+					".github/renovate.json",
+					".github/renovate.json5",
+					".renovaterc",
+					".renovaterc.json",
 				},
+				url = "https://docs.renovatebot.com/renovate-schema",
 			},
-			Lua = {
-				cmd = { "lua-language-server" },
-				filetypes = { "lua" },
-				runtime = {
-					version = "LuaJIT",
-					path = vim.split(package.path, ";"),
-				},
-				completion = { enable = true, callSnippet = "Both" },
-				diagnostics = {
-					enable = true,
-					globals = { "vim", "describe" },
-					disable = { "lowercase-global" },
-				},
-				workspace = {
-					library = {
-						vim.api.nvim_get_runtime_file("", true),
-						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-						[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-						[vim.fn.expand("/usr/share/awesome/lib")] = true,
-					},
-					-- adjust these two values if your performance is not optimal
-					maxPreload = 2000,
-					preloadFileSize = 1000,
-				},
-				telemetry = { enable = false },
+			{
+				fileMatch = { "tsconfig*.json" },
+				url = "https://json.schemastore.org/tsconfig.json",
 			},
-			yaml = {
-				schemaStore = {
-					enable = true,
-					url = "https://www.schemastore.org/api/json/catalog.json",
+			{
+				fileMatch = {
+					".prettierrc",
+					".prettierrc.json",
+					"prettier.config.json",
 				},
-				schemas = {
-					kubernetes = "*.yaml",
-					["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-					["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-					["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-					["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
-					["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-					["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
-					["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-					["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-					["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
-					["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-					["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-					["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
-				},
-				format = { enabled = false },
-				validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
-				completion = true,
-				hover = false,
+				url = "https://json.schemastore.org/prettierrc.json",
+			},
+			{
+				description = "OpenApi config",
+				fileMatch = { "*api*.json" },
+				url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
 			},
 		},
-		flags = { debounce_text_changes = 150 },
-	})
-end
+	},
+	marksman = {},
+	sqlls = {},
+	emmet_ls = {
+		filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+		init_options = {
+			html = {
+				options = {
+					-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+					["bem.enabled"] = true,
+				},
+			},
+		},
+	},
+	astro = {},
+	tailwindcss = {},
+	sumneko_lua = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
+}
+
+-- Setup neovim lua configuration
+require("neodev").setup()
+
+require("mason").setup()
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require("mason-lspconfig")
+
+mason_lspconfig.setup({
+	ensure_installed = vim.tbl_keys(servers),
+})
+
+mason_lspconfig.setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+			on_attach = function(client)
+				-- disable formatting for LSP clients as this is handled by null-ls
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+				local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+				for type, icon in pairs(signs) do
+					local hl = "DiagnosticSign" .. type
+					vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+				end
+				vim.diagnostic.config({ virtual_text = false })
+
+				if client.server_capabilities.documentFormattingProvider then
+					vim.cmd([[augroup Format]])
+					vim.cmd([[autocmd! * <buffer>]])
+					vim.cmd([[autocmd BufWritePost <buffer> lua require'lsp.formatting'.format()]])
+					vim.cmd([[augroup END]])
+				end
+
+				if server_name == "tsserver" then
+					require("nvim-lsp-ts-utils").setup({})
+				end
+
+				map("n", "<leader>dd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				map("n", "<leader>d", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+
+				vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
+			end,
+			settings = servers[server_name],
+			before_init = function(_, config)
+				if server_name == "pyright" then
+					config.settings.python.pythonPath = get_python_path(config.root_dir)
+				end
+			end,
+		})
+	end,
+})
 
 vim.lsp.handlers["textDocument/hover"] = function(...)
 	vim.lsp.with(vim.lsp.handlers.hover, {
