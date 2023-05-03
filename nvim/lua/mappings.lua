115042f72ecdc5594d49951fdf5add47d16e2964
James Best <james.best@breedr.co>
2023-05-03 21:59:45 +0100

    Changed back to telescope references

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -49,6 +49,7 @@ map("n", "<leader>?", ":CheatSH<CR>", opts)
 --Telescope
 map("n", "<leader>b", ":Telescope buffers preview=true<CR>", opts)
 map("n", "<leader>f", ":Telescope live_grep preview=true<CR>", opts)
+map("n", "<leader>dr", ":Telescope lsp_references<CR>", opts)
 map("n", "<C-p>", ":Telescope git_files preview=true<CR>", opts)
 map("n", "<leader>g", ":Telescope git_status preview=true<CR>", opts)
 map("n", "<leader>de", ":Telescope diagnostics<CR>", opts)
--- nvim/lua/plugins/lsp-saga.lua
+++ nvim/lua/plugins/lsp-saga.lua
@@ -11,7 +11,7 @@ return {
 		{ "<leader>dp", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "LspSaga Prev Diagnositics" },
 		{ "<leader>gh", "<cmd>Lspsaga hover_doc<CR>", desc = "LspSaga Hover doc" },
 		{ "<leader>rn", "<cmd>Lspsaga rename<CR>", desc = "LspSaga Rename" },
-		{ "<leader>dr", "<cmd>Lspsaga lsp_finder<CR>", desc = "LspSaga Finder" },
+		-- { "<leader>dr", "<cmd>Lspsaga lsp_finder<CR>", desc = "LspSaga Finder" },
 		{ "<leader>dd", "<cmd>Lspsaga goto_definition<CR>", desc = "LspSaga Goto definition" },
 		{ "<leader>dp", "<cmd>Lspsaga peek_definition<CR>", desc = "LspSaga Peek definition" },
 	},
