a1499b5561b57dc158f20df3d94707b9d0c9a823
James Best <james.best@breedr.co>
2023-06-01 17:37:42 +0100

    Customise catpucin

--- nvim/lua/plugins/my-themes.lua
+++ nvim/lua/plugins/my-themes.lua
@@ -77,6 +77,31 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				transparent_background = true,
+				custom_highlights = function(colors)
+					return {
+						ColorColumn = { bg = colors.pink },
+
+						-- Blend colours against the "base" background
+						CursorLine = { bg = colors.sapphire, blend = 10 },
+						StatusLine = { fg = colors.flamingo, bg = colors.flamingo, blend = 10 },
+
+						FloatBorder = { fg = colors.surface0, bg = colors.surface0 },
+						NormalFloat = { bg = colors.surface0 },
+						FloatTitle = { bg = colors.surface0 },
+
+						TelescopeTitle = { fg = colors.teal, bold = true },
+						TelescopePromptNormal = { bg = colors.surface0 },
+						TelescopePromptBorder = { fg = colors.surface0, bg = colors.surface0 },
+						TelescopeResultsNormal = { fg = colors.text, bg = colors.mantle },
+						TelescopeResultsBorder = { fg = colors.mantle, bg = colors.mantle },
+						TelescopePreviewNormal = { fg = colors.text, bg = colors.crust },
+						TelescopePreviewBorder = { fg = colors.crust, bg = colors.crust },
+
+						NoiceCmdLinePrompt = { fg = colors.sapphire, bold = true },
+						NoiceCmdlinePopup = { fg = colors.sapphire, bg = colors.surface0 },
+						NoiceCmdlinePopupBorder = { fg = colors.surface0, bg = colors.surface0 },
+					}
+				end,
 			})
 		end,
 	},
@@ -257,12 +282,6 @@ return {
 			vim.g.hardhacker_hide_tilde = 1
 			vim.g.hardhacker_keyword_italic = 1
 		end,
-		config = function()
-			FloatBorder = { fg = "#282433", bg = "#282433" }
-			NoiceCmdLinePrompt = { fg = "#282433", bold = true }
-			NoiceCmdlinePopup = { fg = "iris", bg = "nc" }
-			-- NoiceCmdlinePopup = { fg = "iris", bg = "nc" }
-			-- NoiceCmdlinePopupBorder = { fg = "nc", bg = "nc" }
-		end,
+		config = function() end,
 	},
 }
