406918fd14d1b77158cc3ebc1002f5ddf94af2ba
James Best <james.best@breedr.co>
2023-08-04 16:00:34 +0100

    Set up rust tools

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -19,6 +19,7 @@ return {
 					"go",
 					"gomod",
 					"gosum",
+					"toml",
 				}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 				indent = {
 					enable = true,
