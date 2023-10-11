e9c187debc729d5bb7540df080770c2030ad3bdc
James Best <james.best@breedr.co>
2023-10-11 06:35:58 +0100

    More linting stuff

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -96,3 +96,16 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 		vim.diagnostic.open_float(nil, opts)
 	end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
