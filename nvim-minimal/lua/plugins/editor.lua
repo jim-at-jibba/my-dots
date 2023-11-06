3f649e3cf7ba5aa353733c166d85dff4a9a1c29e
James Best <james.best@breedr.co>
2023-11-06 16:38:21 +0000

    Remove unused packages

--- nvim-minimal/lua/plugins/editor.lua
+++ nvim-minimal/lua/plugins/editor.lua
@@ -988,19 +988,8 @@ return {
 				--       enable = true
 				--   }
 				-- },
-				rainbow = {
-					enable = true,
-					-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-					extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-					max_file_lines = nil, -- Do not enable for files with more than n lines, int
-					-- colors = {}, -- table of hex strings
-					-- termcolors = {} -- table of colour name strings
-				},
 				autotag = {
 					enable = true,
-				},
-				matchup = {
-					enable = true, -- mandatory, false will disable the whole extension
 				},
 			})
 		end,
