33805359e81592a1cbab241e51ad1052618f685e
James Best <james.best@breedr.co>
2023-11-07 13:46:28 +0000

    Read ai and toggleterm plugins

--- nvim-minimal/lua/plugins/editor.lua
+++ nvim-minimal/lua/plugins/editor.lua
@@ -1064,4 +1064,144 @@ return {
 			{ "<leader>4", "<cmd>lua require('harpoon.ui').nav_file(4)<CR>", desc = "Navigate to file 4 in Harpoon" },
 		},
 	},
+	--toggleterm
+	{
+		"akinsho/toggleterm.nvim",
+		event = "VeryLazy",
+		config = function()
+			local toggleterm = require("toggleterm")
+			toggleterm.setup({
+				open_mapping = [[<c-\>]],
+				shade_filetypes = { "none" },
+				shade_terminals = true,
+				shading_factor = "1",
+				size = function(term)
+					if term.direction == "horizontal" then
+						return 15
+					elseif term.direction == "vertical" then
+						return vim.o.columns * 0.4
+					end
+				end,
+			})
+
+			function _G.set_terminal_keymaps()
+				local opts = { buffer = 0 }
+				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+			end
+
+			-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+			local Terminal = require("toggleterm.terminal").Terminal
+
+			local lazygit = Terminal:new({
+				cmd = "lazygit",
+				dir = "git_dir",
+				direction = "float",
+				float_opts = {
+					border = "double",
+				},
+				-- function to run on opening the terminal
+				on_open = function(term)
+					vim.cmd("startinsert!")
+					vim.api.nvim_buf_set_keymap(
+						term.bufnr,
+						"n",
+						"q",
+						"<cmd>close<CR>",
+						{ noremap = true, silent = true }
+					)
+				end,
+				-- function to run on closing the terminal
+				on_close = function(term)
+					vim.cmd("startinsert!")
+				end,
+			})
+
+			function _lazygit_toggle()
+				lazygit:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>gs",
+				"<cmd>lua _lazygit_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
+
+			local pio_monitor = Terminal:new({
+				cmd = "pio device monitor",
+				dir = "git_dir",
+				direction = "float",
+				float_opts = {
+					border = "double",
+				},
+				-- function to run on opening the terminal
+				on_open = function(term)
+					-- vim.cmd("startinsert!")
+					vim.api.nvim_buf_set_keymap(
+						term.bufnr,
+						"n",
+						"q",
+						"<cmd>close<CR>",
+						{ noremap = true, silent = true }
+					)
+				end,
+				-- function to run on closing the terminal
+				on_close = function(term)
+					vim.cmd("startinsert!")
+				end,
+			})
+
+			function _pio_monitor_toggle()
+				pio_monitor:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>pm",
+				"<cmd>lua _pio_monitor_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
+
+			local verticalTerm = Terminal:new({
+				dir = "git_dir",
+				direction = "vertical",
+				float_opts = {
+					border = "double",
+				},
+				-- function to run on opening the terminal
+				on_open = function(term)
+					vim.cmd("startinsert!")
+					vim.api.nvim_buf_set_keymap(
+						term.bufnr,
+						"n",
+						"q",
+						"<cmd>close<CR>",
+						{ noremap = true, silent = true }
+					)
+				end,
+				-- function to run on closing the terminal
+				on_close = function(term)
+					vim.cmd("startinsert!")
+				end,
+			})
+
+			function _vertical_toggle()
+				verticalTerm:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>vt",
+				"<cmd>lua _vertical_toggle()<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
 }
