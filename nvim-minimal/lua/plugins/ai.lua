3980d81ae6352056331f82aa48baad73aa39b3d1
James Best <james.best@breedr.co>
2023-11-14 15:19:39 +0000

    Reorganise config

---
+++ nvim-minimal/lua/plugins/ai.lua
@@ -0,0 +1,228 @@
+return {
+	--ai
+	{
+		"jackMort/ChatGPT.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("chatgpt").setup({
+				yank_register = "+",
+				edit_with_instructions = {
+					diff = false,
+					keymaps = {
+						close = "<C-c>",
+						accept = "<C-y>",
+						toggle_diff = "<C-d>",
+						toggle_settings = "<C-o>",
+						cycle_windows = "<Tab>",
+						use_output_as_input = "<C-i>",
+					},
+				},
+				chat = {
+					welcome_message = "What do you want?",
+					loading_text = "Loading, please wait ...",
+					question_sign = "", -- 🙂
+					answer_sign = "ﮧ", -- 🤖
+					max_line_length = 120,
+					sessions_window = {
+						border = {
+							style = "rounded",
+							text = {
+								top = " Sessions ",
+							},
+						},
+						win_options = {
+							winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+						},
+					},
+					keymaps = {
+						close = { "<C-c>" },
+						yank_last = "<C-y>",
+						yank_last_code = "<C-k>",
+						scroll_up = "<C-u>",
+						scroll_down = "<C-d>",
+						new_session = "<C-n>",
+						cycle_windows = "<Tab>",
+						cycle_modes = "<C-f>",
+						next_message = "<C-j>",
+						prev_message = "<C-k>",
+						select_session = "<Space>",
+						rename_session = "r",
+						delete_session = "d",
+						draft_message = "<C-d>",
+						edit_message = "e",
+						delete_message = "d",
+						toggle_settings = "<C-o>",
+						toggle_message_role = "<C-r>",
+						toggle_system_role_open = "<C-s>",
+						stop_generating = "<C-x>",
+					},
+				},
+				popup_layout = {
+					default = "center",
+					center = {
+						width = "80%",
+						height = "80%",
+					},
+					right = {
+						width = "30%",
+						width_settings_open = "50%",
+					},
+				},
+				popup_window = {
+					border = {
+						highlight = "FloatBorder",
+						style = "rounded",
+						text = {
+							top = " ChatGPT ",
+						},
+					},
+					win_options = {
+						wrap = true,
+						linebreak = true,
+						foldcolumn = "1",
+						winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+					},
+					buf_options = {
+						filetype = "markdown",
+					},
+				},
+				system_window = {
+					border = {
+						highlight = "FloatBorder",
+						style = "rounded",
+						text = {
+							top = " SYSTEM ",
+						},
+					},
+					win_options = {
+						wrap = true,
+						linebreak = true,
+						foldcolumn = "2",
+						winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+					},
+				},
+				popup_input = {
+					prompt = "  ",
+					border = {
+						highlight = "FloatBorder",
+						style = "rounded",
+						text = {
+							top_align = "center",
+							top = " Prompt ",
+						},
+					},
+					win_options = {
+						winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+					},
+					submit = "<C-Enter>",
+					submit_n = "<Enter>",
+					max_visible_lines = 20,
+				},
+				settings_window = {
+					border = {
+						style = "rounded",
+						text = {
+							top = " Settings ",
+						},
+					},
+					win_options = {
+						winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+					},
+				},
+				openai_params = {
+					model = "gpt-3.5-turbo",
+					-- model = "gpt-4",
+					frequency_penalty = 0,
+					presence_penalty = 0,
+					max_tokens = 3000,
+					temperature = 0,
+					top_p = 1,
+					n = 1,
+				},
+				openai_edit_params = {
+					model = "gpt-3.5-turbo",
+					frequency_penalty = 0,
+					presence_penalty = 0,
+					temperature = 0,
+					top_p = 1,
+					n = 1,
+				},
+				use_openai_functions_for_edits = false,
+				actions_paths = {},
+				show_quickfixes_cmd = "Trouble quickfix",
+				predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
+			})
+		end,
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{
+		"piersolenski/wtf.nvim",
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"gw",
+				mode = { "n" },
+				function()
+					require("wtf").ai()
+				end,
+				desc = "Debug diagnostic with AI",
+			},
+			{
+				mode = { "n" },
+				"gW",
+				function()
+					require("wtf").search()
+				end,
+				desc = "Search diagnostic with Google",
+			},
+		},
+	},
+	-- copilot
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				filetypes = {
+					yaml = false,
+					markdown = false,
+					help = false,
+					gitcommit = false,
+					gitrebase = false,
+					hgcommit = false,
+					svn = false,
+					cvs = false,
+					go = false,
+					rust = false,
+					elixir = false,
+					["."] = true,
+				},
+				panel = {
+					enabled = false,
+				},
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					debounce = 75,
+					keymap = {
+						accept = "<C-e>",
+						accept_word = false,
+						accept_line = false,
+						next = "<M-]>",
+						prev = "<M-[>",
+						dismiss = "<C-]>",
+					},
+				},
+			})
+		end,
+	},
+}
---
+++ nvim-minimal/lua/plugins/lsp.lua
@@ -0,0 +1,358 @@
+return {
+	-- LSP
+	{
+		"rmagatti/goto-preview",
+		keys = {
+			{
+				"<leader>dpd",
+				"<cmd>lua require('goto-preview').goto_preview_definition()<CR>",
+				desc = "Preview definition",
+			},
+			{
+				"<leader>dpr",
+				"<cmd>lua require('goto-preview').goto_preview_references()<CR>",
+				desc = "Preview definition",
+			},
+			{
+				"<esc>",
+				"<cmd>lua require('goto-preview').close_all_win()<CR>",
+				desc = "Preview definition",
+			},
+		},
+		config = function()
+			require("goto-preview").setup({})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				opts = function(_, opts)
+					vim.list_extend(opts.ensure_installed, {
+						"delve",
+						"gotests",
+						"golangci-lint",
+						"gofumpt",
+						"goimports",
+						"golangci-lint-langserver",
+						"impl",
+						"gomodifytags",
+						"iferr",
+						"gotestsum",
+						"intelephense",
+						"phpactor",
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		keys = {
+			{ "<leader>dd", "<cmd>lua vim.lsp.buf.definition()<CR>", desc = "Go to definition (LSP)" },
+			{ "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", desc = "Code action (LSP)" },
+			{ "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", desc = "Rename (LSP)" },
+			{ "<leader>gh", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "Show docs (LSP)" },
+			{
+				"<leader>dl",
+				"<cmd>lua vim.diagnostic.open_float({ border = 'rounded', max_width = 100 })<CR>",
+				desc = "Show live diagnostics (LSP)",
+			},
+			{
+				"<leader>dn",
+				"<cmd>lua vim.diagnostic.goto_next({ border = 'rounded', max_width = 100 })<CR>",
+				desc = "Show live diagnostics (LSP)",
+			},
+			{
+				"<leader>dp",
+				"<cmd>lua vim.diagnostic.goto_prev({ border = 'rounded', max_width = 100 })<CR>",
+				desc = "Show live diagnostics (LSP)",
+			},
+		},
+		config = function()
+			local util = require("lspconfig/util")
+			local nvim_lsp = require("lspconfig")
+			local path = util.path
+			local function get_python_path(workspace)
+				-- Use activated virtualenv.
+				if vim.env.VIRTUAL_ENV then
+					return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+				end
+				-- Find and use virtualenv in workspace directory.
+				for _, pattern in ipairs({ "*", ".*" }) do
+					local match = vim.fn.glob(path.join(workspace, pattern, "pyvenv.cfg"))
+					if match ~= "" then
+						return path.join(path.dirname(match), "bin", "python")
+					end
+				end
+				-- Fallback to system Python.
+				return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+			end
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
+			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+			local clangd_capabilities = capabilities
+			clangd_capabilities.textDocument.semanticHighlighting = true
+			clangd_capabilities.offsetEncoding = "utf-8"
+
+			local servers = {
+				"gopls",
+				"pyright",
+				"prismals",
+				"html",
+				"cssls",
+				"lua_ls",
+				"yamlls",
+				"jsonls",
+				"marksman",
+				"sqlls",
+				"emmet_ls",
+				"astro",
+				"tailwindcss",
+				"arduino_language_server",
+				"clangd",
+				"rust_analyzer",
+				"golangci_lint_ls",
+				"svelte",
+				"dockerls",
+				"graphql",
+				"intelephense",
+				"phpactor",
+			}
+
+			-- Use a loop to conveniently call 'setup' on multiple servers
+			for _, lsp in ipairs(servers) do
+				local lsp_capabilities
+				if lsp == "clangd" then
+					lsp_capabilities = clangd_capabilities
+				else
+					lsp_capabilities = capabilities
+				end
+
+				-- https://www.reddit.com/r/neovim/s/TgMwZpSVWj
+				-- lsp_capabilities.didChangeWatchedFiles.dynamicRegistration = true
+
+				nvim_lsp[lsp].setup({
+					on_attach = function(client, bufnr)
+						-- disable formatting for LSP clients as this is handled by null-ls
+						client.server_capabilities.documentFormattingProvider = false
+						client.server_capabilities.documentRangeFormattingProvider = false
+						local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+						for type, icon in pairs(signs) do
+							local hl = "DiagnosticSign" .. type
+							vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+						end
+
+						local config = {
+							-- disable virtual text
+							virtual_text = false,
+							-- show signs
+							signs = { active = signs },
+							update_in_insert = false,
+							underline = true,
+							severity_sort = true,
+						}
+						vim.diagnostic.config(config)
+
+						if client.server_capabilities.documentFormattingProvider then
+							vim.cmd([[augroup Format]])
+							vim.cmd([[autocmd! * <buffer>]])
+							vim.cmd([[autocmd BufWritePost <buffer> lua require'lsp.formatting'.format()]])
+							vim.cmd([[augroup END]])
+						end
+
+						-- if lsp == "tsserver" then
+						-- 	require("nvim-lsp-ts-utils").setup({})
+						-- end
+					end,
+					before_init = function(_, config)
+						if lsp == "pyright" then
+							print(get_python_path(config.root_dir))
+							config.settings.python.pythonPath = get_python_path(config.root_dir)
+							config.settings.python.venvPath = "/Users/jamesbest/.pyenv/versions/"
+							config.settings.python.venv = "breedr-api"
+						end
+					end,
+					capabilities = lsp_capabilities,
+					settings = {
+						graphql = {
+							filetypes = { "graphql", "javascript", "typescript", "typescriptreact", "javascriptreact" },
+						},
+						html = {
+							filetypes = {
+								"tmpl",
+								"html",
+								"javascript",
+								"javascriptreact",
+								"javascript.jsx",
+								"typescript",
+								"typescriptreact",
+								"typescript.tsx",
+								"svelte",
+							},
+						},
+						emmet_ls = {
+							filetypes = {
+								"html",
+								"typescriptreact",
+								"javascriptreact",
+								"css",
+								"sass",
+								"scss",
+								"less",
+								"svelte",
+							},
+							init_options = {
+								html = {
+									options = {
+										-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+										["bem.enabled"] = true,
+									},
+								},
+							},
+						},
+						golangci_lint_ls = {},
+						gopls = {
+							analyses = {
+								unusedparams = true,
+							},
+							hints = {
+								assignVariableTypes = true,
+								compositeLiteralFields = true,
+								compositeLiteralTypes = true,
+								constantValues = true,
+								functionTypeParameters = true,
+								parameterNames = true,
+								rangeVariableTypes = true,
+							},
+							staticcheck = true,
+							semanticTokens = true,
+						},
+						json = {
+							format = { enabled = false },
+							schemas = {
+								{
+									description = "ESLint config",
+									fileMatch = { ".eslintrc.json", ".eslintrc" },
+									url = "http://json.schemastore.org/eslintrc",
+								},
+								{
+									description = "Package config",
+									fileMatch = { "package.json" },
+									url = "https://json.schemastore.org/package",
+								},
+								{
+									description = "Packer config",
+									fileMatch = { "packer.json" },
+									url = "https://json.schemastore.org/packer",
+								},
+								{
+									description = "Renovate config",
+									fileMatch = {
+										"renovate.json",
+										"renovate.json5",
+										".github/renovate.json",
+										".github/renovate.json5",
+										".renovaterc",
+										".renovaterc.json",
+									},
+									url = "https://docs.renovatebot.com/renovate-schema",
+								},
+								{
+									fileMatch = { "tsconfig*.json" },
+									url = "https://json.schemastore.org/tsconfig.json",
+								},
+								{
+									fileMatch = {
+										".prettierrc",
+										".prettierrc.json",
+										"prettier.config.json",
+									},
+									url = "https://json.schemastore.org/prettierrc.json",
+								},
+								{
+									description = "OpenApi config",
+									fileMatch = { "*api*.json" },
+									url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json",
+								},
+							},
+						},
+						lua_ls = {
+							cmd = { "lua-language-server" },
+							filetypes = { "lua" },
+							runtime = {
+								version = "LuaJIT",
+								path = vim.split(package.path, ";"),
+							},
+							completion = { enable = true, callSnippet = "Both" },
+							diagnostics = {
+								enable = true,
+								globals = { "vim", "describe" },
+								disable = { "lowercase-global" },
+							},
+							workspace = {
+								library = {
+									vim.api.nvim_get_runtime_file("", true),
+									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+									[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+									[vim.fn.expand("/usr/share/awesome/lib")] = true,
+								},
+								-- adjust these two values if your performance is not optimal
+								maxPreload = 2000,
+								preloadFileSize = 1000,
+							},
+							telemetry = { enable = false },
+						},
+						yaml = {
+							schemaStore = {
+								enable = true,
+								url = "https://www.schemastore.org/api/json/catalog.json",
+							},
+							schemas = {
+								kubernetes = "*.yaml",
+								["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+								["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+								["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+								["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+								["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+								["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+								["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+								["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+								["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+								["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+								["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+								["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+							},
+							format = { enabled = false },
+							validate = false, -- TODO: conflicts between Kubernetes resources and kustomization.yaml
+							completion = true,
+							hover = false,
+						},
+					},
+					flags = { debounce_text_changes = 150 },
+				})
+			end
+
+			vim.lsp.handlers["textDocument/hover"] = function(...)
+				vim.lsp.with(vim.lsp.handlers.hover, {
+					border = "rounded",
+				})(...)
+			end
+
+			vim.lsp.handlers["textDocument/signatureHelp"] = function(...)
+				vim.lsp.with(vim.lsp.handlers.signature_help, {
+					border = "rounded",
+				})(...)
+			end
+		end,
+	},
+	{ "jose-elias-alvarez/nvim-lsp-ts-utils" },
+}
