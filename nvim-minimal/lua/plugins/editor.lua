8b1c951aabc563fe2c9c4813f928f615bb3d1957
James Best <james.best@breedr.co>
2023-11-17 07:49:12 +0000

    Disable toggleterm

--- nvim-minimal/lua/plugins/editor.lua
+++ nvim-minimal/lua/plugins/editor.lua
@@ -522,6 +522,7 @@ return {
 	{
 		"akinsho/toggleterm.nvim",
 		event = "VeryLazy",
+		enabled = false,
 		config = function()
 			local toggleterm = require("toggleterm")
 			toggleterm.setup({
@@ -661,6 +662,7 @@ return {
 
 	{
 		"folke/noice.nvim",
+		enabled = false,
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 		},
@@ -757,6 +759,27 @@ return {
 		},
 		config = function()
 			require("inc_rename").setup()
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup({
+				notification = {
+					window = {
+						normal_hl = "Comment", -- Base highlight group in the notification window
+						winblend = 0, -- Background color opacity in the notification window
+						border = "none", -- Border around the notification window
+						zindex = 45, -- Stacking priority of the notification window
+						max_width = 0, -- Maximum width of the notification window
+						max_height = 0, -- Maximum height of the notification window
+						x_padding = 1, -- Padding from right edge of window boundary
+						y_padding = 0, -- Padding from bottom edge of window boundary
+						align_bottom = true, -- Whether to bottom-align the notification window
+						relative = "editor", -- What the notification window position is relative to
+					},
+				},
+			})
 		end,
 	},
 }
--- nvim-minimal/lua/plugins/themes.lua
+++ nvim-minimal/lua/plugins/themes.lua
@@ -2,12 +2,8 @@ return {
 	-- themes
 	{
 		"oxfist/night-owl.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd.colorscheme("night-owl")
-		end,
+		lazy = true,
+		event = "VeryLazy",
 	},
 
 	{
