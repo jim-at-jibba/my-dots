63f66d74e5e6bf182238feb10196b8521be7fada
James Best <james.best@breedr.co>
2023-12-29 05:59:58 +0000

    Change file explorer

--- nvim-minimal/lua/plugins/editor.lua
+++ nvim-minimal/lua/plugins/editor.lua
@@ -342,73 +342,17 @@ return {
 		end,
 	},
 	{ "nvim-telescope/telescope-ui-select.nvim" },
-	-- nvim-tree
 	{
-		"nvim-tree/nvim-tree.lua",
-		keys = {
-			{ "<leader><leader>1", ":NvimTreeToggle<CR>", desc = "Toggle NVIM tree", silent = true, remap = false },
-			{ "<leader>r", ":NvimTreeRefresh<CR>", desc = "Refresh NVIM tree" },
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
 		},
-		event = "VeryLazy",
 		config = function()
-			require("nvim-tree").setup({
-				on_attach = on_attach,
-				disable_netrw = true,
-				hijack_cursor = true,
-				view = {
-					side = "right",
-					width = 40,
-				},
-				renderer = {
-					indent_markers = { enable = true },
-					group_empty = true,
-					special_files = {},
-					icons = {
-						show = {
-							git = true,
-							folder = true,
-							file = true,
-							folder_arrow = true,
-						},
-						glyphs = {
-							default = "",
-							symlink = "",
-							git = {
-								unstaged = "!",
-								staged = "+",
-								unmerged = "",
-								renamed = "➜",
-								untracked = "?",
-								deleted = "",
-								ignored = "◌",
-							},
-							folder = {
-								arrow_open = "",
-								arrow_closed = "",
-								default = "",
-								open = "",
-								empty = "",
-								empty_open = "",
-								symlink = "",
-								symlink_open = "",
-							},
-						},
-					},
-				},
-				filters = { custom = { ".git/", "node_modules" } },
-				actions = {
-					open_file = {
-						window_picker = {
-							exclude = {
-								filetype = {
-									"packer",
-								},
-								buftype = { "nofile", "terminal", "help" },
-							},
-						},
-					},
-				},
-			})
+			vim.keymap.set("n", "<leader><leader>1", ":Neotree toggle show filesystem right<CR>", { silent = true })
+			vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", { silent = true })
 		end,
 	},
 	-- treesitter
