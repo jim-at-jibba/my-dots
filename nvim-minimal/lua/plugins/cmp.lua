e550fadea804f5aed36a3b4d033a42b45dd53e7d
James Best <james.best@breedr.co>
2024-01-12 15:23:10 +0000

    Cmp formatting

--- nvim-minimal/lua/plugins/cmp.lua
+++ nvim-minimal/lua/plugins/cmp.lua
@@ -57,12 +57,29 @@ return {
 			})
 
 			cmp.setup({
+				window = {
+					completion = {
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+						col_offset = -3,
+						side_padding = 0,
+					},
+				},
 				formatting = {
-					format = lspkind.cmp_format({
-						with_text = true,
-						maxwidth = 50,
-						mode = "symbol",
-					}),
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, item)
+						local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, item)
+
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = " " .. (strings[1] or "") .. " "
+						kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+						return kind
+					end,
+					-- format = lspkind.cmp_format({
+					-- 	with_text = true,
+					-- 	maxwidth = 50,
+					-- 	mode = "symbol",
+					-- }),
 				},
 				experimental = { native_menu = false, ghost_text = false },
 				snippet = {
