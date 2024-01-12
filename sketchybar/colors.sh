85127298632a57e6fb1cc6b90bd9f934571c86ce
James Best <james.best@breedr.co>
2024-01-12 08:08:11 +0000

    Add sketchy bar

---
+++ sketchybar/colors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Color Palette
+export BLACK=0xff393552
+export WHITE=0xffe0def4
+export RED=0xffeb6f92
+export GREEN=0xff3e8fb0
+export BLUE=0xff9ccfd8
+export YELLOW=0xfff6c177
+export ORANGE=0xfff5a97f
+export MAGENTA=0xffc4a7e7
+export GREY=0xff939ab7
+export TRANSPARENT=0x00000000
+
+# General bar colors
+export BAR_COLOR=0xa0232136
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export BACKGROUND_1=0x903c3e4f
+export BACKGROUND_2=0x90494d64
+
+export POPUP_BACKGROUND_COLOR=0xff24273a
+export POPUP_BORDER_COLOR=$WHITE
+
+export SHADOW_COLOR=$BLACK
