c78f58d46489442886318678e8e9867c2dec13a0
James Best <james.best@breedr.co>
2024-01-29 20:10:50 +0000

    Add theme

--- daily-driver/init.lua
+++ daily-driver/init.lua
@@ -1013,6 +1013,89 @@ require("lazy").setup({
 	},
 	{ "jose-elias-alvarez/nvim-lsp-ts-utils" },
 	-- LSP END
+
+	-- THEMES START
+	{
+		"rose-pine/neovim",
+		as = "rose-pine",
+		lazy = true,
+		config = function()
+			require("rose-pine").setup({
+				--- @usage 'auto'|'main'|'moon'|'dawn'
+				variant = "auto",
+				--- @usage 'main'|'moon'|'dawn'
+				dark_variant = "moon",
+				bold_vert_split = false,
+				dim_nc_background = false,
+				disable_background = true,
+				disable_float_background = false,
+				disable_italics = false,
+
+				--- @usage string hex value or named color from rosepinetheme.com/palette
+				groups = {
+					background = "base",
+					background_nc = "_experimental_nc",
+					panel = "surface",
+					panel_nc = "base",
+					border = "highlight_med",
+					comment = "muted",
+					link = "iris",
+					punctuation = "subtle",
+
+					error = "love",
+					hint = "iris",
+					info = "foam",
+					warn = "gold",
+
+					headings = {
+						h1 = "iris",
+						h2 = "foam",
+						h3 = "rose",
+						h4 = "gold",
+						h5 = "pine",
+						h6 = "foam",
+					},
+					-- or set all headings at once
+					-- headings = 'subtle'
+				},
+
+				-- Change specific vim highlight groups
+				-- https://github.com/rose-pine/neovim/wiki/Recipes
+				highlight_groups = {
+					ColorColumn = { bg = "rose" },
+
+					-- Blend colours against the "base" background
+					CursorLine = { bg = "foam", blend = 10 },
+					StatusLine = { fg = "love", bg = "love", blend = 10 },
+
+					FloatBorder = { fg = "surface", bg = "surface" },
+					NormalFloat = { bg = "surface" },
+					FloatTitle = { bg = "surface" },
+
+					TelescopeTitle = { fg = "love", bold = true },
+					TelescopePromptNormal = { bg = "surface" },
+					TelescopePromptBorder = { fg = "surface", bg = "surface" },
+					TelescopeResultsNormal = { fg = "text", bg = "nc" },
+					TelescopeResultsBorder = { fg = "nc", bg = "nc" },
+					TelescopePreviewNormal = { fg = "text", bg = "overlay" },
+					TelescopePreviewBorder = { fg = "overlay", bg = "overlay" },
+
+					-- NoiceCmdLinePrompt = { fg = "foam", bold = true },
+					-- NoiceCmdlinePopup = { fg = "iris", bg = "nc" },
+					-- NoiceCmdlinePopupBorder = { fg = "nc", bg = "nc" },
+					-- NoiceMini = { fg = "iris", bg = "overlay" },
+
+					-- -- TitleString = { fg = "rose", bg = "surface" },
+					-- -- TitleIcon = { fg = "surface", bg = "surface" },
+					-- DiagnosticBorder = { fg = "surface", bg = "surface" },
+					-- DiagnosticNormal = { bg = "surface" },
+					-- DiagnosticShowNormal = { fg = "surface", bg = "surface" },
+					-- DiagnosticShowBorder = { bg = "surface" },
+				},
+			})
+		end,
+	},
+	-- THEMES END
 }, {
 	defaults = { version = false },
 	install = { colorscheme = { "rose-pine" } },
@@ -1021,6 +1104,8 @@ require("lazy").setup({
 -- Vim mappings, see lua/config/which.lua for more mappings
 local map = vim.keymap.set
 local opts = { silent = true, noremap = true }
+vim.cmd("set background=dark")
+vim.cmd.colorscheme("rose-pine")
 
 --Remap space as leader key
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
