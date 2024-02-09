c4a64f7c947993abcbffb3942dcb2142cf0d3c66
James Best <james.best@breedr.co>
2024-02-09 07:40:52 +0000

    Add a few plugins

--- daily-driver/init.lua
+++ daily-driver/init.lua
@@ -753,7 +753,14 @@ require("lazy").setup({
 				formatters_by_ft = {
 					lua = { "stylua" },
 					-- Conform will run multiple formatters sequentially
-					python = { "ruff", "black", "isort" },
+					-- python = { "ruff_format", "black", "isort" },
+					python = function(bufnr)
+						if require("conform").get_formatter_info("ruff_format", bufnr).available then
+							return { "ruff_format" }
+						else
+							return { "isort", "black" }
+						end
+					end,
 					-- Use a sub-list to run only the first available formatter
 					javascript = { { "prettierd", "prettier" } },
 					typescript = { { "prettierd", "prettier" } },
@@ -777,6 +784,7 @@ require("lazy").setup({
 				typescript = { "eslint" },
 				typescriptreact = { "eslint" },
 				lua = { "luacheck" },
+				python = { "ruff" },
 			}
 		end,
 	},
@@ -1170,10 +1178,62 @@ require("lazy").setup({
 			end
 		end,
 	},
+	{
+		"dgagn/diagflow.nvim",
+		event = "LspAttach",
+		opts = {},
+	},
 	{ "jose-elias-alvarez/nvim-lsp-ts-utils" },
 	-- LSP END
 
 	-- THEMES START
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = true,
+		config = function()
+			require("nightfox").setup({
+				options = {
+					transparent = true, -- Disable setting background
+					terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+					dim_inactive = false, -- Non focused panes set to alternative background
+				},
+				groups = {
+					all = {
+						ColorColumn = { bg = "palette.bg1" },
+
+						-- Blend colours against the "base" background
+						CursorLine = { bg = "bg2" },
+						StatusLine = { fg = "pink", bg = "pink", blend = "10" },
+
+						normalfloat = { bg = "bg2" },
+						FloatBorder = { fg = "palette.bg2", bg = "palette.bg2" },
+						FloatTitle = { bg = "palette.bg2" },
+
+						TelescopeTitle = { fg = "pink", bold = "true" },
+						TelescopePromptNormal = { bg = "bg2" },
+						TelescopePromptBorder = { fg = "bg2", bg = "bg2" },
+						TelescopeResultsNormal = { fg = "white", bg = "bg0" },
+						TelescopeResultsBorder = { fg = "bg0", bg = "bg0" },
+						TelescopePreviewNormal = { fg = "white", bg = "bg3" },
+						TelescopePreviewBorder = { fg = "bg3", bg = "bg3" },
+
+						NoiceCmdLinePrompt = { fg = "orange", bold = "true" },
+						NoiceCmdlinePopup = { fg = "palette.magenta", bg = "palette.bg2" },
+						NoiceCmdlinePopupBorder = { fg = "palette.bg2", bg = "palette.bg2" },
+						NoiceMini = { fg = "orange", bg = "bg2" },
+						-- NoiceLspProgressSpinner = { bg = colors.theme.ui.bg },
+						-- NoiceLspProgressClient = { bg = colors.theme.ui.bg },
+						-- NoiceLspProgressTitle = { bg = colors.theme.ui.bg },
+
+						-- DiagnosticBorder = { fg = "surface", bg = "surface" },
+						DiagnosticNormal = { bg = "bg2", fg = "bg2" },
+						-- DiagnosticShowNormal = { fg = "surface", bg = "surface" },
+						-- DiagnosticShowBorder = { bg = "surface" },
+					},
+				},
+			})
+		end,
+	},
 	{
 		"rose-pine/neovim",
 		as = "rose-pine",
@@ -1414,6 +1474,93 @@ require("lazy").setup({
 			end,
 		},
 	},
+	-- GIT START
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { hl = "GitSignsAdd", text = "▍", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+					change = {
+						hl = "GitSignsChange",
+						text = "▍",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+					delete = {
+						hl = "GitSignsDelete",
+						text = "▸",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					topdelete = {
+						hl = "GitSignsDelete",
+						text = "▾",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					changedelete = {
+						hl = "GitSignsChange",
+						text = "▍",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "<CR>", function()
+						if vim.wo.diff then
+							return "<CR>"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					map("n", "<backspace>", function()
+						if vim.wo.diff then
+							return "<backspace>"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					-- Actions
+					map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+					map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+					map("n", "<leader>hS", gs.stage_buffer)
+					map("n", "<leader>hu", gs.undo_stage_hunk)
+					map("n", "<leader>hR", gs.reset_buffer)
+					map("n", "<leader>hp", gs.preview_hunk)
+					map("n", "<leader>hb", function()
+						gs.blame_line({ full = true })
+					end)
+					map("n", "<leader>gb", gs.toggle_current_line_blame)
+					map("n", "<leader>dt", gs.diffthis)
+					map("n", "<leader>hD", function()
+						gs.diffthis("~")
+					end)
+					map("n", "<leader>td", gs.toggle_deleted)
+
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
+	-- GIT END
 }, {
 	defaults = { version = false },
 	install = { colorscheme = { "rose-pine" } },
