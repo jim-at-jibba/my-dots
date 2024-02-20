960f1d9a78ee4493768f9c13dd3f558351492de0
James Best <james.best@breedr.co>
2024-02-20 19:26:21 +0000

    Remap telescope

--- lazy/lua/plugins/telescope.lua
+++ lazy/lua/plugins/telescope.lua
@@ -1,15 +1,9 @@
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
-    -- disable the keymap to grep files
-    {"<leader>/", false},
-    -- change a keymap
+    { "<leader>/", false },
     { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    -- add a keymap to browse plugin files
-    {
-      "<leader>fp",
-      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-      desc = "Find Plugin File",
-    },
+    { "<leader><space>", false },
+    { "<C-p>", ":Telescope git_files preview=true<CR>", desc = "Fuzzy search (Telescope)" },
   },
 }
