bdcde153f4af12cd23d9c1fe8e9f3d5bb1be224d
James Best <james.best@breedr.co>
2024-02-22 14:32:15 +0000

    Add harpoon

---
+++ lazy/lua/plugins/harpoon.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = "VeryLazy",
+    config = function()
+      local harpoon = require("harpoon")
+      -- REQUIRED
+      harpoon:setup()
+
+      vim.keymap.set("n", "<leader>hj", function()
+        harpoon:list():append()
+      end)
+      vim.keymap.set("n", "<leader>hh", function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
+
+      vim.keymap.set("n", "<leader>1", function()
+        harpoon:list():select(1)
+      end)
+      vim.keymap.set("n", "<leader>2", function()
+        harpoon:list():select(2)
+      end)
+      vim.keymap.set("n", "<leader>3", function()
+        harpoon:list():select(3)
+      end)
+      vim.keymap.set("n", "<leader>4", function()
+        harpoon:list():select(4)
+      end)
+    end,
+  },
+}
