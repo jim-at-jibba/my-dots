8123728558e9809fbe73e74858619692d5aa024b
James Best <james.best@breedr.co>
2024-02-22 14:34:31 +0000

    More telescope configuration

--- lazy/lua/plugins/telescope.lua
+++ lazy/lua/plugins/telescope.lua
@@ -5,5 +5,7 @@ return {
     { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<leader><space>", false },
     { "<C-p>", ":Telescope git_files preview=true<CR>", desc = "Fuzzy search (Telescope)" },
+    { "<leader>b", ":Telescope buffers preview=true<CR>", desc = "View Buffers (Telescope)" },
+    { "<leader>g", ":Telescope git_status preview=true<CR>", desc = "Git status (Telescope)" },
   },
 }
