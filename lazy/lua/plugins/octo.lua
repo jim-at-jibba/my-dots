baa655a292b42d57219c4f71eb7e4ee37b47b3ab
James Best <james.best@breedr.co>
2024-03-13 15:08:57 +0000

    Add Octo and copilot chat

---
+++ lazy/lua/plugins/octo.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "pwntester/octo.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      -- OR 'ibhagwan/fzf-lua',
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup()
+    end,
+  },
+}
