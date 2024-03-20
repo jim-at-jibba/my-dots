91587bd5a73950edf828a978234426cd35213224
James Best <james.best@breedr.co>
2024-03-20 14:41:48 +0000

    New snippets

--- lazy/lua/plugins/luasnip.lua
+++ lazy/lua/plugins/luasnip.lua
@@ -2,6 +2,7 @@ local ls = require("luasnip")
 
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
 return {
   "L3MON4D3/LuaSnip",
@@ -22,6 +23,15 @@ return {
       }),
       s("fixme", {
         t("// FIXME(JGB): "),
+      }),
+      s("perf-start", {
+        t("const startTime = performance.now();"),
+      }),
+      s("perf-end", {
+        t("const endTime = performance.now();"),
+        t("console.log('"),
+        i(1, "perf name"),
+        t("', endTime - startTime)"),
       }),
     }, {
       key = "all",
