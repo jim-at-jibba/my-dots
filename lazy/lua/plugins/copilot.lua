a513172ba1ee3ba711669f317bb0a9665460cbd1
James Best <james.best@breedr.co>
2024-03-21 07:50:42 +0000

    feat(copilot): add keybindings and window options

--- lazy/lua/plugins/copilot.lua
+++ lazy/lua/plugins/copilot.lua
@@ -7,9 +7,20 @@ return {
       { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChatToggle<CR>", desc = "Toggle copilot chat window" },
+      { "<leader>cR", "<cmd>CopilotChatReset<CR>", desc = "Reset copilot chat window" },
+    },
     opts = {
       debug = false, -- Enable debugging
       -- See Configuration section for rest
+      window = {
+        layout = "float",
+        relative = "cursor",
+        width = 1,
+        height = 0.4,
+        row = 1,
+      },
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
