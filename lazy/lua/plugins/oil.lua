ecfea2f30729441d7840050d38ecefbc7320a18e
James Best <james.best@breedr.co>
2024-03-21 07:51:36 +0000

    feat(copilot): add keybindings and window options
    This commit introduces keybindings for toggling and resetting the
    copilot chat window. It also adds window options for layout, relative
    position, width, height, and row. These changes enhance the usability
    and customization of the copilot plugin.

--- lazy/lua/plugins/oil.lua
+++ lazy/lua/plugins/oil.lua
@@ -4,5 +4,8 @@ return {
     opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader><leader>1", "<cmd>Oil<CR>", desc = "Open oil" },
+    },
   },
 }
