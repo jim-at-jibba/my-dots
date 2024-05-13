1cee636eac599c676bc6d80e6ece07bdd70ab66d
James Best <james.best@breedr.co>
2024-05-13 09:59:15 +0100

    Add chainsaw

---
+++ lazy/lua/plugins/debug_print.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "chrisgrieser/nvim-chainsaw",
+    config = true,
+    keys = {
+      { "<leader>l", "<cmd>lua require('chainsaw').variableLog()<CR>", { desc = "Chainsaw variable log" } },
+      { "<leader>lo", "<cmd>lua require('chainsaw').objectLog()<CR>", { desc = "Chainsaw object log" } },
+      { "<leader>lm", "<cmd>lua require('chainsaw').messageLog()<CR>", { desc = "Chainsaw message log" } },
+      { "<leader>lb", "<cmd>lua require('chainsaw').beepLog()<CR>", { desc = "Chainsaw beep log" } },
+      { "<leader>lt", "<cmd>lua require('chainsaw').timeLog()<CR>", { desc = "Chainsaw time log" } },
+      { "<leader>ld", "<cmd>lua require('chainsaw').removeLogs()<CR>", { desc = "Chainsaw remove logs" } },
+    },
+  },
+}
