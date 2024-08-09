265fe965484431a3893330ee55df083cfae423dc
James Best <james.best@breedr.co>
2024-08-09 05:22:19 +0100

    Continue working on copy commmit

--- shellscripts/copy-committed.sh
+++ shellscripts/copy-committed.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Define the source and destination directories
-src_dir="/path/to/source/directory"
-dest_dir="/path/to/destination/directory"
+src_dir="/Users/jamesbest/code/other/wiki"
+dest_dir="/Users/jamesbest/code/work/fe-til"
 
 # Get the list of files to be committed
 files_to_commit=$(git diff --cached --name-only)
+echo "what" $files_to_commit
 
 # Loop over the files
 for file in $files_to_commit; do
+  echo "file" $file
   # Check if the file is in the source directory
-  if [[ $file == $src_dir* ]]; then
+  if [[ $src_dir$file == $src_dir* ]]; then
     # Copy the file to the destination directory
-    cp $file $dest_dir/${file#$src_dir/}
+    echo "Copying $src_dir/$file to $dest_dir/${file#*/}"
+    cp $src_dir/$file $dest_dir/${file#*/}
   fi
 done
 
@@ -22,5 +25,9 @@ cd $dest_dir
 # Add all the copied files to the staging area
 git add .
 
+# use ai to generate commit message
+msg="$(git diff --staged | sgpt "Generate a concise git commit message that summarizes the key changes. Stay high-level and combine smaller changes to overarching topics. Skip describing any reformatting changes. For example, 'Add new feature, refactor existing code, and fix bug")"
+PREFIXED_MSG="${msg}"
+echo ${PREFIXED_MSG}
 # Commit the changes
-git commit -m "Auto commit from pre-commit hook"
+git commit -m "${PREFIXED_MSG}"
