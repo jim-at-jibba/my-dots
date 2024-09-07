ea85842f020e59930c92a4d3af1728b4fe85d27d
James Best <james.best@breedr.co>
2024-09-07 16:10:00 +0100

    **chore: refactor shell scripts for improved fabric usage*

--- shellscripts/fabric/yt-rate.sh
+++ shellscripts/fabric/yt-rate.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Run fabric -l and pipe the output to gum select
-gum input | xargs -I {} yt {} | fabric --pattern rate_content
+# Get the video URL from user input
+video_url=$(gum input --placeholder "Video URL...")
+
+# Display the entered URL and ask for confirmation
+echo "Entered URL: $video_url"
+if gum confirm "Do you want to rate this video?"; then
+  yt "$video_url" | fabric --pattern rate_content
+else
+  echo "Operation cancelled."
+fi
--- shellscripts/fabric/yt-summary.sh
+++ shellscripts/fabric/yt-summary.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-gum input | xargs -I {} yt {} | fabric --pattern extract_wisdom
+# Set up trap to handle SIGINT (Ctrl+C)
+trap 'exit 130' SIGINT
+
+# Get the video URL from user input
+video_url=$(gum input --placeholder "Video URL...")
+dest="$HOME/MyBrain/MyBrain/00_Fleeting"
+# Display the entered URL and ask for confirmation
+echo "Entered URL: $video_url"
+if gum confirm "Do you want to extract wisdom from this video?"; then
+
+  title=$(yt-dlp --get-title "$video_url")
+  sanitized_title=$(echo "$title" | tr -cd '[:alnum:][:space:]' | tr '[:space:]' '_')
+  output_file="$dest/${sanitized_title}.md"
+  yt "$video_url" | fabric --pattern extract_wisdom >"$output_file"
+  echo "Wisdom extracted and saved to: $output_file"
+else
+  echo "Operation cancelled."
+fi
