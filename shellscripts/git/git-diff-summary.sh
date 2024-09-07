fae1f35db5df22aae26f9cf63150a9587e408fdc
James Best <james.best@breedr.co>
2024-09-07 16:15:12 +0100

    Improve script

--- shellscripts/git/git-diff-summary.sh
+++ shellscripts/git/git-diff-summary.sh
@@ -3,7 +3,11 @@
 git diff --staged | fabric --pattern summarize_git_diff >commit_msg.tmp
 if [ -s commit_msg.tmp ]; then
   commit_msg=$(cat commit_msg.tmp | gum write --width 80 --height 20)
-  gum confirm "Commit with message: $commit_msg" && git commit -m "$commit_msg"
+  if gum confirm "Commit with message: $commit_msg"; then
+    git commit -m "$commit_msg"
+  else
+    echo "Commit aborted."
+  fi
   rm commit_msg.tmp
 else
   echo "No changes to commit."
