05f9d6c8515b72ecb2d1103446baa8252411a227
James Best <james.best@breedr.co>
2024-10-09 11:44:09 +0100

    Fix incorrect method call

--- lazy/lua/plugins/debug_print.lua
+++ lazy/lua/plugins/debug_print.lua
@@ -7,16 +7,18 @@ return {
         logStatements = {
           objectLog = {
             javascript = 'console.log("%s %s:", JSON.stringify(%s, null, 2));',
+            typescript = 'console.log("%s %s:", JSON.stringify(%s, null, 2));',
+            typescriptreact = 'console.log("%s %s:", JSON.stringify(%s, null, 2));',
           },
         },
-        beepEmojis = { "🔵 1:", "🟩 2:", "⭐ 3:", "⭕ 4:", "💜 5:", "🔲 6:" },
+        logEmojis = { "🔵 1:", "🟩 2:", "⭐ 3:", "⭕ 4:", "💜 5:", "🔲 6:" },
       })
     end,
     keys = {
       { "<leader>l", "<cmd>lua require('chainsaw').variableLog()<CR>", { desc = "Chainsaw variable log" } },
       { "<leader>lo", "<cmd>lua require('chainsaw').objectLog()<CR>", { desc = "Chainsaw object log" } },
       { "<leader>lm", "<cmd>lua require('chainsaw').messageLog()<CR>", { desc = "Chainsaw message log" } },
-      { "<leader>lb", "<cmd>lua require('chainsaw').beepLog()<CR>", { desc = "Chainsaw beep log" } },
+      { "<leader>lb", "<cmd>lua require('chainsaw').emojiLog()<CR>", { desc = "Chainsaw beep log" } },
       { "<leader>lt", "<cmd>lua require('chainsaw').timeLog()<CR>", { desc = "Chainsaw time log" } },
       { "<leader>ld", "<cmd>lua require('chainsaw').removeLogs()<CR>", { desc = "Chainsaw remove logs" } },
     },
