23fd48e5d6150807f7b75d538b4ecad48975c8ea
James Best <james.best@breedr.co>
2024-10-17 11:47:26 +0100

    Make script better

--- shellscripts/fabric/yt-summary.sh
+++ shellscripts/fabric/yt-summary.sh
@@ -3,6 +3,29 @@
 # Set up trap to handle SIGINT (Ctrl+C)
 trap 'exit 130' SIGINT
 
+# Function to generate header with metadata
+generate_header() {
+  local url="$1"
+  local title="$2"
+  local d=$(date +"%Y-%m-%d")
+
+  echo "---"
+  echo "title: \"$title\""
+  echo "url: $url"
+  echo "date: $d"
+  echo "type: video_summary"
+  echo "tags:"
+  echo "  - 🪴weedy"
+  echo "  - 🤖generated"
+  echo "source:" "$url"
+  echo "---"
+  echo
+  echo "# $title"
+  echo
+  echo "## Summary"
+  echo
+}
+
 # Get the video URL from user input
 video_url=$(gum input --placeholder "Video URL...")
 dest="$HOME/MyBrain/MyBrain/00_Fleeting"
@@ -12,8 +35,12 @@ if gum confirm "Do you want to extract wisdom from this video?"; then
 
   title=$(yt-dlp --get-title "$video_url")
   sanitized_title=$(echo "$title" | tr -cd '[:alnum:][:space:]' | tr '[:space:]' '_')
-  output_file="$dest/🤖_${sanitized_title}.md"
-  yt "$video_url" | fabric --pattern extract_wisdom >"$output_file"
+  output_file="$dest/${sanitized_title}.md"
+
+  # Generate header and write to file
+  generate_header "$video_url" "$title" >"$output_file"
+
+  yt "$video_url" | fabric --pattern extract_wisdom >>"$output_file"
   echo "Wisdom extracted and saved to: $output_file"
 else
   echo "Operation cancelled."
