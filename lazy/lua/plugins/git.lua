26d2be8bf6a1adc75571c4039297b2cfdeb6f7c0
James Best <james.best@breedr.co>
2024-11-22 17:18:27 +0000

    feat(plugins): add Neogit for enhanced Git integration
    - Install NeogitOrg/neogit plugin
    - Configure dependencies: plenary.nvim, diffview.nvim, and telescope.nvim
    - Enable default configuration with `config = true`

--- lazy/lua/plugins/git.lua
+++ lazy/lua/plugins/git.lua
@@ -1,3 +1,14 @@
 return {
   { "akinsho/git-conflict.nvim", version = "*", config = true },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true,
+  },
 }
