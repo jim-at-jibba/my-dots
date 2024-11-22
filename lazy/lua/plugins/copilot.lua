5eab19f30a34ce15cebe27499df6af19bf3353f4
James Best <james.best@breedr.co>
2024-11-22 17:16:58 +0000

    refactor(plugins): replace avante.nvim with codecompanion.nvim
    - Remove avante.nvim plugin configuration
    - Add codecompanion.nvim plugin with anthropic adapter
    - Update lazy-lock.json with new dependencies

--- lazy/lua/plugins/copilot.lua
+++ lazy/lua/plugins/copilot.lua
@@ -1,50 +1,50 @@
 return {
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     -- add any opts here
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = "make",
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "stevearc/dressing.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       "HakonHarnes/img-clip.nvim",
+  --       event = "VeryLazy",
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
   {
     "supermaven-inc/supermaven-nvim",
     config = function()
@@ -57,58 +57,58 @@ return {
       })
     end,
   },
-  -- {
-  --   "olimorris/codecompanion.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "nvim-telescope/telescope.nvim", -- Optional
-  --     {
-  --       "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
-  --     },
-  --   },
-  --   opts = {},
-  --   keys = {
-  --     { "<leader>aa", "<cmd>CodeCompanionChat<CR>", desc = "Toggle codecompanion chat window" },
-  --     { "<leader>ai", "<cmd>CodeCompanion<CR>", desc = "Toggle codecompanion inline" },
-  --     { "<leader>ac", "<cmd>CodeCompanionActions<CR>", desc = "CodeCompanion actions" },
-  --     { "<leader>ga", "<cmd>CodeCompanionChat Add<CR>", desc = "CodeCompanion Add" },
-  --   },
-  --   config = function()
-  --     require("codecompanion").setup({
-  --       display = {
-  --         diff = {
-  --           provider = "mini_diff",
-  --         },
-  --       },
-  --       adapters = {
-  --         anthropic = function()
-  --           return require("codecompanion.adapters").extend("anthropic", {
-  --             schema = {
-  --               model = {
-  --                 default = "claude-3-5-sonnet-20240620",
-  --               },
-  --             },
-  --           })
-  --         end,
-  --       },
-  --       strategies = {
-  --         chat = {
-  --           adapter = "anthropic",
-  --         },
-  --         inline = {
-  --           adapter = "anthropic",
-  --         },
-  --         agent = {
-  --           adapter = "anthropic",
-  --         },
-  --       },
-  --     })
-  --
-  --     -- Expand 'cc' into 'CodeCompanion' in the command line
-  --     vim.cmd([[cab cc CodeCompanion]])
-  --   end,
-  -- },
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp", -- Optional: For using slash commands and variables in the chat buffer
+      "nvim-telescope/telescope.nvim", -- Optional: For using slash commands
+      { "MeanderingProgrammer/render-markdown.nvim", ft = { "markdown", "codecompanion" } }, -- Optional: For prettier markdown rendering
+      { "stevearc/dressing.nvim", opts = {} }, -- Optional: Improves `vim.ui.select`
+    },
+    keys = {
+      { "<leader>aa", "<cmd>CodeCompanionChat<CR>", desc = "Toggle codecompanion chat window" },
+      { "<leader>ae", "<cmd>CodeCompanion<CR>", desc = "Toggle codecompanion inline" },
+      { "<leader>ac", "<cmd>CodeCompanionActions<CR>", desc = "CodeCompanion actions" },
+      { "<leader>ga", "<cmd>CodeCompanionChat Add<CR>", desc = "CodeCompanion Add" },
+    },
+    config = function()
+      require("codecompanion").setup({
+        display = {
+          diff = {
+            provider = "mini_diff",
+          },
+        },
+        adapters = {
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              schema = {
+                model = {
+                  default = "claude-3-5-sonnet-20240620",
+                },
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = {
+            adapter = "anthropic",
+          },
+          inline = {
+            adapter = "anthropic",
+          },
+          agent = {
+            adapter = "anthropic",
+          },
+        },
+      })
+
+      -- Expand 'cc' into 'CodeCompanion' in the command line
+      vim.cmd([[cab cc CodeCompanion]])
+    end,
+  },
   -- {
   --   "zbirenbaum/copilot.lua",
   --   event = "VeryLazy",
