c73ef2bff682147bee49b0f5d76700185239c8db
James Best <james.best@breedr.co>
2024-12-12 06:36:15 +0000

    Use snacks now

--- lazy/lua/plugins/noice.lua
+++ lazy/lua/plugins/noice.lua
@@ -51,9 +51,9 @@ return {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
         enabled = true, -- enables the Noice messages UI
-        view = "mini", -- default view for messages
-        view_error = "mini", -- view for errors
-        view_warn = "mini", -- view for warnings
+        -- view = "mini", -- default view for messages
+        -- view_error = "mini", -- view for errors
+        -- view_warn = "mini", -- view for warnings
         view_history = "messages", -- view for :messages
         view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
       },
