de902fe4349bd27a46cabf4e640c08a05ac11594
James Best <james.best@breedr.co>
2025-05-08 05:29:36 +0100

    Some moving and shaking

---
+++ kick/lua/rohm/plugins/temp.lua
@@ -0,0 +1,466 @@
+return {
+  {
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+      'lewis6991/gitsigns.nvim',
+      opts = {
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      },
+    },
+    { -- Useful plugin to show you pending keybinds.
+      'folke/which-key.nvim',
+      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+      opts = {
+        delay = 0,
+        icons = {
+          mappings = vim.g.have_nerd_font,
+          keys = vim.g.have_nerd_font and {} or {
+            Up = '<Up> ',
+            Down = '<Down> ',
+            Left = '<Left> ',
+            Right = '<Right> ',
+            C = '<C-…> ',
+            M = '<M-…> ',
+            D = '<D-…> ',
+            S = '<S-…> ',
+            CR = '<CR> ',
+            Esc = '<Esc> ',
+            ScrollWheelDown = '<ScrollWheelDown> ',
+            ScrollWheelUp = '<ScrollWheelUp> ',
+            NL = '<NL> ',
+            BS = '<BS> ',
+            Space = '<Space> ',
+            Tab = '<Tab> ',
+            F1 = '<F1>',
+            F2 = '<F2>',
+            F3 = '<F3>',
+            F4 = '<F4>',
+            F5 = '<F5>',
+            F6 = '<F6>',
+            F7 = '<F7>',
+            F8 = '<F8>',
+            F9 = '<F9>',
+            F10 = '<F10>',
+            F11 = '<F11>',
+            F12 = '<F12>',
+          },
+        },
+
+        -- Document existing key chains
+        spec = {
+          { '<leader>s', group = '[S]earch' },
+          { '<leader>t', group = '[T]oggle' },
+          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        },
+      },
+    },
+
+    { -- Autoformat
+      'stevearc/conform.nvim',
+      event = { 'BufWritePre' },
+      cmd = { 'ConformInfo' },
+      keys = {
+        {
+          '<leader>f',
+          function()
+            require('conform').format { async = true, lsp_format = 'fallback' }
+          end,
+          mode = '',
+          desc = '[F]ormat buffer',
+        },
+      },
+      opts = {
+        notify_on_error = false,
+        format_on_save = function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for languages that don't
+          -- have a well standardized coding style. You can add additional
+          -- languages here or re-enable it for the disabled ones.
+          local disable_filetypes = { c = true, cpp = true }
+          if disable_filetypes[vim.bo[bufnr].filetype] then
+            return nil
+          else
+            return {
+              timeout_ms = 500,
+              lsp_format = 'fallback',
+            }
+          end
+        end,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          -- Conform can also run multiple formatters sequentially
+          -- python = { "isort", "black" },
+          --
+          -- You can use 'stop_after_first' to run the first available formatter from the list
+          javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        },
+      },
+    },
+
+    { -- Autocompletion
+      'saghen/blink.cmp',
+      event = 'VimEnter',
+      version = '1.*',
+      dependencies = {
+        -- Snippet Engine
+        {
+          'L3MON4D3/LuaSnip',
+          version = '2.*',
+          build = (function()
+            -- Build Step is needed for regex support in snippets.
+            -- This step is not supported in many windows environments.
+            -- Remove the below condition to re-enable on windows.
+            if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+              return
+            end
+            return 'make install_jsregexp'
+          end)(),
+          dependencies = {
+            -- `friendly-snippets` contains a variety of premade snippets.
+            --    See the README about individual language/framework/plugin snippets:
+            --    https://github.com/rafamadriz/friendly-snippets
+            {
+              'rafamadriz/friendly-snippets',
+              config = function()
+                require('luasnip.loaders.from_vscode').lazy_load()
+              end,
+            },
+          },
+          opts = {},
+        },
+        'folke/lazydev.nvim',
+      },
+      --- @module 'blink.cmp'
+      --- @type blink.cmp.Config
+      opts = {
+        keymap = {
+          -- 'default' (recommended) for mappings similar to built-in completions
+          --   <c-y> to accept ([y]es) the completion.
+          --    This will auto-import if your LSP supports it.
+          --    This will expand snippets if the LSP sent a snippet.
+          -- 'super-tab' for tab to accept
+          -- 'enter' for enter to accept
+          -- 'none' for no mappings
+          --
+          -- For an understanding of why the 'default' preset is recommended,
+          -- you will need to read `:help ins-completion`
+          --
+          -- No, but seriously. Please read `:help ins-completion`, it is really good!
+          --
+          -- All presets have the following mappings:
+          -- <tab>/<s-tab>: move to right/left of your snippet expansion
+          -- <c-space>: Open menu or open docs if already open
+          -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+          -- <c-e>: Hide menu
+          -- <c-k>: Toggle signature help
+          --
+          -- See :h blink-cmp-config-keymap for defining your own keymap
+          preset = 'default',
+
+          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        },
+
+        appearance = {
+          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+          -- Adjusts spacing to ensure icons are aligned
+          nerd_font_variant = 'mono',
+        },
+
+        completion = {
+          -- By default, you may press `<c-space>` to show the documentation.
+          -- Optionally, set `auto_show = true` to show the documentation after a delay.
+          documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        },
+
+        sources = {
+          default = { 'lsp', 'path', 'snippets', 'lazydev' },
+          providers = {
+            lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          },
+        },
+
+        snippets = { preset = 'luasnip' },
+
+        -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+        -- which automatically downloads a prebuilt binary when enabled.
+        --
+        -- By default, we use the Lua implementation instead, but you may enable
+        -- the rust implementation via `'prefer_rust_with_warning'`
+        --
+        -- See :h blink-cmp-config-fuzzy for more information
+        fuzzy = { implementation = 'lua' },
+
+        -- Shows a signature help window while you type arguments for a function
+        signature = { enabled = true },
+      },
+    },
+    {
+      'rose-pine/neovim',
+      name = 'rose-pine',
+      config = function()
+        require('rose-pine').setup {
+          --- @usage 'auto'|'main'|'moon'|'dawn'
+          variant = 'dawn',
+          --- @usage 'main'|'moon'|'dawn'
+          dark_variant = 'moon',
+          bold_vert_split = false,
+          dim_nc_background = false,
+          disable_background = true,
+          disable_float_background = false,
+          disable_italics = true,
+
+          --- @usage string hex value or named color from rosepinetheme.com/palette
+          groups = {
+            background = 'base',
+            background_nc = '_experimental_nc',
+            panel = 'surface',
+            panel_nc = 'base',
+            border = 'highlight_med',
+            comment = 'muted',
+            link = 'iris',
+            punctuation = 'subtle',
+
+            error = 'love',
+            hint = 'iris',
+            info = 'foam',
+            warn = 'gold',
+
+            headings = {
+              h1 = 'iris',
+              h2 = 'foam',
+              h3 = 'rose',
+              h4 = 'gold',
+              h5 = 'pine',
+              h6 = 'foam',
+            },
+            -- or set all headings at once
+            -- headings = 'subtle'
+          },
+
+          -- Change specific vim highlight groups
+          -- https://github.com/rose-pine/neovim/wiki/Recipes
+          highlight_groups = {
+            -- ColorColumn = { bg = "rose" },
+
+            -- Blend colours against the "base" background
+            CursorLine = { bg = 'foam', blend = 10 },
+            StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+
+            FloatBorder = { fg = 'surface', bg = 'surface' },
+            NormalFloat = { bg = 'surface' },
+            FloatTitle = { bg = 'surface' },
+
+            TelescopeTitle = { fg = 'love', bold = true },
+            TelescopePromptNormal = { bg = 'surface' },
+            TelescopePromptBorder = { fg = 'surface', bg = 'surface' },
+            TelescopeResultsNormal = { fg = 'text', bg = 'nc' },
+            TelescopeResultsBorder = { fg = 'nc', bg = 'nc' },
+            TelescopePreviewNormal = { fg = 'text', bg = 'overlay' },
+            TelescopePreviewBorder = { fg = 'overlay', bg = 'overlay' },
+
+            NoiceCmdLinePrompt = { fg = 'foam', bold = true },
+            NoiceCmdlinePopup = { fg = 'iris', bg = 'nc' },
+            NoiceCmdlinePopupBorder = { fg = 'nc', bg = 'nc' },
+            NoiceMini = { fg = 'iris', bg = 'overlay' },
+          },
+        }
+        vim.cmd 'colorscheme rose-pine'
+      end,
+    },
+
+    -- Highlight todo, notes, etc in comments
+    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+    { -- Collection of various small independent plugins/modules
+      'echasnovski/mini.nvim',
+      config = function()
+        -- Better Around/Inside textobjects
+        --
+        -- Examples:
+        --  - va)  - [V]isually select [A]round [)]paren
+        --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+        --  - ci'  - [C]hange [I]nside [']quote
+        require('mini.ai').setup { n_lines = 500 }
+
+        -- Add/delete/replace surroundings (brackets, quotes, etc.)
+        --
+        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+        -- - sd'   - [S]urround [D]elete [']quotes
+        -- - sr)'  - [S]urround [R]eplace [)] [']
+        require('mini.surround').setup()
+
+        -- Simple and easy statusline.
+        --  You could remove this setup call if you don't like it,
+        --  and try some other statusline plugin
+        local statusline = require 'mini.statusline'
+        -- set use_icons to true if you have a Nerd Font
+        statusline.setup { use_icons = vim.g.have_nerd_font }
+
+        -- You can configure sections in the statusline by overriding their
+        -- default behavior. For example, here we set the section for
+        -- cursor location to LINE:COLUMN
+        ---@diagnostic disable-next-line: duplicate-set-field
+        statusline.section_location = function()
+          return '%2l:%-2v'
+        end
+
+        -- ... and there is more!
+        --  Check out: https://github.com/echasnovski/mini.nvim
+      end,
+    },
+    { -- Highlight, edit, and navigate code
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate',
+      main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      opts = {
+        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+          enable = true,
+          -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+          --  If you are experiencing weird indenting issues, add the language to
+          --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+          additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<C-space>',
+            node_incremental = '<C-space>',
+            scope_incremental = false,
+            node_decremental = '<BS>',
+          },
+        },
+      },
+      -- There are additional nvim-treesitter modules that you can use to interact
+      -- with nvim-treesitter. You should go explore a few and see what interests you:
+      --
+      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    },
+    -- Oil
+    {
+      'stevearc/oil.nvim',
+      opts = {},
+      -- Optional dependencies
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      keys = {
+        { '-', '<cmd>Oil<CR>', desc = 'Open oil' },
+      },
+    },
+    {
+      'supermaven-inc/supermaven-nvim',
+      config = function()
+        require('supermaven-nvim').setup {}
+      end,
+    },
+    {
+      'folke/snacks.nvim',
+      priority = 1000,
+      lazy = false,
+      opts = {
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        explorer = { enabled = false },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = false },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+      },
+      keys = {
+        {
+          '<leader>z',
+          function()
+            Snacks.zen()
+          end,
+          desc = 'Toggle [Z]en Mode',
+        },
+        {
+          '<leader>.',
+          function()
+            Snacks.scratch()
+          end,
+          desc = 'Toggle Scratch Buffer',
+        },
+        {
+          '<leader>S',
+          function()
+            Snacks.scratch.select()
+          end,
+          desc = 'Select [S]cratch Buffer',
+        },
+        {
+          '<leader>n',
+          function()
+            Snacks.notifier.show_history()
+          end,
+          desc = '[N]otification History',
+        },
+        {
+          '<leader>bd',
+          function()
+            Snacks.bufdelete()
+          end,
+          desc = '[B]uffer [D]elete',
+        },
+        {
+          '<leader>gg',
+          function()
+            Snacks.lazygit()
+          end,
+          desc = 'Lazygit',
+        },
+        {
+          '<leader>un',
+          function()
+            Snacks.notifier.hide()
+          end,
+          desc = 'Dismiss All [N]otifications',
+        },
+      },
+    },
+
+    -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
+    -- init.lua. If you want these files, they are in the repository, so you can just download them and
+    -- place them in the correct locations.
+
+    -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
+    --
+    --  Here are some example plugins that I've included in the Kickstart repository.
+    --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+    --
+    -- require 'kickstart.plugins.debug',
+    -- require 'kickstart.plugins.indent_line',
+    -- require 'kickstart.plugins.lint',
+    -- require 'kickstart.plugins.autopairs',
+    -- require 'kickstart.plugins.neo-tree',
+    -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+    -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+    --    This is the easiest way to modularize your config.
+    --
+    --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+    -- { import = 'custom.plugins' },
+    --
+    -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+    -- Or use telescope!
+    -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+    -- you can continue same window with `<space>sr` which resumes last telescope search
+  },
+}
