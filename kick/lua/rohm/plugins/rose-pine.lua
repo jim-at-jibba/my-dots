b3d6eea070845f672f75903d7ae1f57d92ac53be
James Best <james.best@breedr.co>
2025-10-02 07:04:34 +0100

    New themes

--- kick/lua/rohm/plugins/rose-pine.lua
+++ kick/lua/rohm/plugins/rose-pine.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       require('rose-pine').setup {
         --- @usage 'auto'|'main'|'moon'|'dawn'
-        variant = 'dawn',
+        variant = 'moon',
         --- @usage 'main'|'moon'|'dawn'
         dark_variant = 'moon',
         bold_vert_split = false,
