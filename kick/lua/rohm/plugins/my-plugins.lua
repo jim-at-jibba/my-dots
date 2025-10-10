ddd30c1fbc466fc09d0d4d2a7393921694e14398
James Best <james.best@breedr.co>
2025-10-10 10:46:13 +0100

    Add new plugin

--- kick/lua/rohm/plugins/my-plugins.lua
+++ kick/lua/rohm/plugins/my-plugins.lua
@@ -2,4 +2,22 @@ return {
   {
     -- dir = '~/code/other/termogotchi-nvim',
   },
+  {
+    dir = '~/code/other/nvim-redraft/',
+    config = function()
+      require('nvim-redraft').setup {
+        llm = {
+          provider = 'xai', -- "openai" or "anthropic"
+          -- model = 'claude-3-7-sonnet-latest', -- Model name (optional, uses provider default if omitted)
+          timeout = 30000,
+        },
+        debug = true,
+        -- Optional configuration
+        keybindings = {
+          visual_edit = '<leader>ae',
+        },
+      }
+    end,
+    build = 'cd ts && npm install && npm run build',
+  },
 }
