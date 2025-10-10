c1958cd7e38b82b12f4d6e5a121365c15cbcf1e7
James Best <james.best@breedr.co>
2025-10-10 10:45:57 +0100

    Comment out markdown lint - super annoying

--- kick/lua/rohm/plugins/lint.lua
+++ kick/lua/rohm/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         javascript = { 'eslint' },
         javascriptreact = { 'eslint' },
         typescript = { 'eslint' },
