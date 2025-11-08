cc41715e8620446d93a3e04f8c8c0007ac666b47
James Best <james.best@breedr.co>
2025-11-08 15:27:04 +0000

    Replace undotree with atone

---
+++ kick/lua/rohm/plugins/atone.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'XXiaoA/atone.nvim',
+    cmd = 'Atone',
+    opts = {}, -- your configuration here
+    keys = {
+      { '<leader>u', '<cmd>Atone toggle<cr>', { desc = 'Open UndoTree' } },
+    },
+  },
+}
