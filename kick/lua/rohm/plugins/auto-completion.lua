c46bba29fd5e1ab5b9977ab3336af34e52e02a6a
James Best <james.best@breedr.co>
2025-12-18 16:14:01 +0000

    Remove sidekick tab handling

--- kick/lua/rohm/plugins/auto-completion.lua
+++ kick/lua/rohm/plugins/auto-completion.lua
@@ -10,15 +10,15 @@ return {
         ['<CR>'] = { 'accept', 'fallback' },
         ['<C-\\>'] = { 'hide', 'fallback' },
         ['<C-n>'] = { 'select_next', 'show' },
-        -- ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
-        ['<Tab>'] = {
-          'snippet_forward',
-          function() -- sidekick next edit suggestion
-            return require('sidekick').nes_jump_or_apply()
-          end,
-          'snippet_forward',
-          'fallback',
-        },
+        ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+        -- ['<Tab>'] = {
+        --   -- 'snippet_forward',
+        --   -- function() -- sidekick next edit suggestion
+        --   --   return require('sidekick').nes_jump_or_apply()
+        --   -- end,
+        --   'snippet_forward',
+        --   'fallback',
+        -- },
         ['<C-p>'] = { 'select_prev' },
         ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
         ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
