e5a6f90adf6646053562f4c75798c4a7b754df0e
James Best <jamesbest@jamesbestsidekickmoneycom-MacBook-Pro.local>
2026-01-05 20:19:17 +0000

    sidekick setup

--- kick/lua/rohm/plugins/treesitter.lua
+++ kick/lua/rohm/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
