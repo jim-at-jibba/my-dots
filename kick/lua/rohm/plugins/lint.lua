d6631269e22e453fd440b31397443d634bde5f72
James Best <james.best@sidekickmoney.com>
2026-01-16 10:25:51 +0000

    Mostly theme

--- kick/lua/rohm/plugins/lint.lua
+++ kick/lua/rohm/plugins/lint.lua
@@ -7,10 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         -- markdown = { 'markdownlint' },
-        javascript = { 'biomejs' },
-        javascriptreact = { 'biomejs' },
-        typescript = { 'biomejs' },
-        typescriptreact = { 'biomejs' },
+        javascript = { 'biomejs', 'eslint' },
+        javascriptreact = { 'biomejs', 'eslint' },
+        typescript = { 'biomejs', 'eslint' },
+        typescriptreact = { 'biomejs', 'eslint' },
         solidity = { 'solhint' },
         python = { 'ruff' },
         java = { 'checkstyle' },
